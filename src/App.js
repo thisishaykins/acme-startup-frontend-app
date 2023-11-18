de28cda331768f8f334acba1acd9a6d0162a1ada
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-11-18 17:20:31 +0100

    Added Invoice App & Router for: InvoiceList & InvoiceDetail

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import InvoiceForm from './components/InvoiceForm';
+import InvoiceList from './components/InvoiceList';
+import InvoiceDetail from './components/InvoiceDetail';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ACME Startup Invoicing App</h1>
       </header>
-    </div>
+      {/* <InvoiceForm /> */}
+      {/* <InvoiceList /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<InvoiceList />} />
+          <Route path="/invoice/:invoiceId" element={<InvoiceDetail />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
 
