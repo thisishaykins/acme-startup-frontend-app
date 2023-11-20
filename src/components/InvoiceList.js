35786265111dbf02b2f94a39d369213808611f83
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-11-20 08:01:13 +0100

    UI fixes on the Invoice Listing...

--- src/components/InvoiceList.js
+++ src/components/InvoiceList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Col, Row } from 'antd';
+import { Card, Col, Row } from 'antd';
 import { getInvoices } from '../services/api';
 import InvoiceForm from './InvoiceForm';
 
@@ -44,32 +44,38 @@ const InvoiceList = () => {
             </Col>
             <Col span={15}>
                 <h2>Invoices</h2>
-                <ul>
+                <Row gutter={16}>
                     {Array.isArray(invoices) && invoices.length > 0 ? (
-                    invoices.map((invoice) => (
-                    <li key={invoice.id}>
-                        <strong>Invoice #{invoice.id}</strong>
-                        <p>Date: {invoice.date}</p>
-                        <p>Customer Name: {invoice.customerName}</p>
-                        <p>Customer Address: {invoice.customerAddress}</p>
-                        <ul>
-                            {invoice.details.map((detail) => (
-                                <li key={detail.id}>
-                                    <p>Product Code: {detail.productCode}</p>
-                                    <p>Product Description: {detail.productDescription}</p>
-                                    <p>Price per Unit: {detail.productPricePerUnit}</p>
-                                    <p>Quantity: {detail.productQuantity}</p>
-                                </li>
-                            ))}
-                        </ul>
-                        {/* Link to the detail view */}
-                        <Link to={`/invoice/${invoice.id}`}>View Details</Link>
-                    </li>
-                    ))
+                        invoices.map((invoice) => (
+                        <Col span={8}>
+                            <Card 
+                                type="inner" 
+                                title={'Invoice ' + invoice.id} 
+                                key={invoice.id} bordered={true}
+                                extra={<Link to={`/invoice/${invoice.id}`}>View Details</Link>}>
+                                <p><small>Date:</small><br/> {invoice.date}</p>
+                                <p><small>Customer Name:</small><br/> {invoice.customerName}</p>
+                                <p><small>Customer Address:<br/> {invoice.customerAddress}</small></p>
+                                <small><strong>Invoice Counts: {invoice.details.length}</strong></small>
+                                {/* <ul>
+                                    {invoice.details.map((detail) => (
+                                        <li key={detail.id}>
+                                            <p>Product Code: {detail.productCode}</p>
+                                            <p>Product Description: {detail.productDescription}</p>
+                                            <p>Price per Unit: {detail.productPricePerUnit}</p>
+                                            <p>Quantity: {detail.productQuantity}</p>
+                                        </li>
+                                    ))}
+                                </ul> */}
+                                {/* Link to the detail view */}
+                                {/* <Link to={`/invoice/${invoice.id}`}>View Details</Link> */}
+                            </Card>
+                        </Col>
+                        ))
                     ) : (
-                    <p>No invoices available</p>
+                        <p>No invoices available. fetching...</p>
                     )}
-                </ul>
+                </Row>
             </Col>
         </Row>
     </section>
